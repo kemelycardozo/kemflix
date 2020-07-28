43571a033bb1a857d2fbe907e893d4399bdc0a27
Kemely Cardozo <kemely.cardozo@programmers.com.br>
2020-07-28 17:24:49 -0300

    Setup inicial do projeto

---
+++ src/components/Menu/components/ButtonLink/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function ButtonLink(props) {
+    // props => {className: "", href: "" }
+    console.log("props: ", props);
+    return (
+    <a className={props.className} href={props.href}>{props.children}</a>
+    );
+}
+export default ButtonLink
